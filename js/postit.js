45c74198809a43fa5764d6b97b82e740467c965a
jaddenki <jaddenki@gmail.com>
2024-01-13 00:20:54 -0500

    Fixed clock placement and added svg folder

--- js/postit.js
+++ js/postit.js
@@ -1,2 +1,21 @@
-function drag(){
+var drag;
+
+function dragMove(id){
+    var element = document.getElementById(id);
+    element.style.position = "absolute";
+    element.onmousedown = function(){
+        drag = element;
+    }
+}
+
+document.onmouseup = function(e){
+    drag = null;
+}
+
+document.onmousemove = function(e){
+    var x = e.pageX;
+    var y = e.pageY;
+
+    drag.style.left = x + "px";
+    drag.style.top = y = "px";
 }
