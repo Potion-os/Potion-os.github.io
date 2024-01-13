ed46aed638e6c93d17e3c56c7fb74a1860592273
Emily Tumacder <emilytmdr@gmail.com>
2024-01-12 22:53:23 -0500

    Clock is working !!!!!!!

--- js/os-bar.js
+++ js/os-bar.js
@@ -1,14 +1,12 @@
-const now = new Date();
-
-function getTime(){
+function getTime() {
+    const now = new Date();
     let currentTime = now.toLocaleTimeString();
-    document.querySelector('#clock-time').textContent = currentTime;
-}
-
-function getDate(){
+    document.querySelector("#clock-time").textContent = currentTime;
+  }
+  function getDate() {
+    const now = new Date();
     let currentDate = now.toLocaleDateString();
-    document.querySelector('#clock-date').textContent = currentDate;
-}
-
-setInterval(getTime, 1000);
-setInterval(getDate, 1000);
+    document.querySelector("#clock-date").textContent = currentDate;
+  }
+  setInterval(getTime, 1000);
+  setInterval(getDate, 1000);
