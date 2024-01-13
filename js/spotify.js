1b3143b088af44d716087ee7f7109db2f1ea460a
Emily Tumacder <emilytmdr@gmail.com>
2024-01-13 13:13:22 -0500

    redirection to spotify oauth

--- js/spotify.js
+++ js/spotify.js
@@ -2,16 +2,20 @@ const clientID = "dbdcca5fc40142e1a745b430bc4b646b";
 const clientSecret = "d26f71e17af743bd8c3cfc677c5008f1";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
-//&& document.getElementById("enter").clicked == true
-if (!code && document.getElementById("enter").clicked == true){
-    console.log("request submitted");
-    redirectToAuthCodeFlow(clientID);
-} 
-else {
-    const accessToken = await getAccessToken(clientID, code);
-    const profile = await fetchProfile(accessToken);
-    console.log(profile);
-    populateUI(profile);
+
+document.getElementById("get-started").addEventListener("click", run);
+
+async function run(){
+    if (!code){
+        console.log("request submitted");
+        redirectToAuthCodeFlow(clientID);
+    } 
+    else {
+        const accessToken = await getAccessToken(clientID, code);
+        const profile = await fetchProfile(accessToken);
+        console.log(profile);
+        populateUI(profile);
+    }
 }
 
 async function redirectToAuthCodeFlow(clientID) {
